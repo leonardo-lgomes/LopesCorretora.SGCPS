793b269cd1e9ed9a8b966024b12838a6ae10e9a2
Douglas Machado <douglas-machado@outlook.com>
2018-03-25 15:48:57 -0300

    adicao de css

--- LopesCorretora.SGCPS.UI/Controllers/ClienteController.cs
+++ LopesCorretora.SGCPS.UI/Controllers/ClienteController.cs
@@ -1,27 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LopesCorretora.SGCPS.Business;
 using LopesCorretora.SGCPS.Models;
 using LopesCorretora.SGCPS.ViewsModels;
-using LopesCorretora.SGCPS.UI;
+
 
 namespace LopesCorretora.SGCPS.UI.Controllers
 {
-    public class HomeController : Controller
+    public class ClienteController : Controller
     {
-        public IActionResult Dashboard()
+        public IActionResult Pesquisa()
         {
-
-            return View();
-        }
-
-        public IActionResult Home()
-        {
-            return View();
+            return View(new PesquisaClienteVM());
         }
     }
 }
---
+++ LopesCorretora.SGCPS.ViewsModels/PesquisaClienteVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LopesCorretora.SGCPS.Models;
+
+namespace LopesCorretora.SGCPS.ViewsModels
+{
+    public class PesquisaClienteVM
+    {
+        public List<PessoaFisicaMOD> LisPessoaFisicaMOD { get; set; }
+        public List<PessoaJuridicaMOD> LisPessoaJuridicaMOD { get; set; }
+    }
+}
